66d4b92692e75d3cef83466d59aeae8ee9828c3e
Lundrim Aliu <aliulundrim22@gmail.com>
2022-07-18 01:44:55 +0200

    Projects Section

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,99 @@
-import React from "react";
+import React, { useEffect, useInsertionEffect, useState } from "react";
+import styled from "styled-components";
+import SectionTittle from "../components/SectionTittle";
+import { MdSearch } from "react-icons/md";
+import ProjectItem from "../components/ProjectItem";
+import ProjectInfo from "../assets/data/projects";
+
+const ProjectsStyles = styled.div`
+  padding: 10rem 0;
+  .projects__allItems {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 5rem;
+    margin-top: 5rem;
+  }
+  .projects__searchbar {
+    position: relative;
+    width: 300px;
+    margin-top: 5rem;
+  }
+  .projects__searchbar input {
+    width: 100%;
+    font-size: 2rem;
+    padding: 0.8rem;
+    color: var(--black);
+    outline: none;
+    border: none;
+  }
+  .projects__searchbar .searchIcon {
+    position: absolute;
+    width: 2rem;
+    right: 1rem;
+  }
+  .projects__searchbar .searchIcon path {
+    color: var(--deep-dark);
+  }
+  @media only screen and (max-width: 768px) {
+    .projects__searchbar,
+    .projects__allItems form,
+    .projects__searchbar input {
+      width: 100%;
+    }
+  }
+`;
 
 export default function Projects() {
+  const [searchText, setSearchText] = useState("");
+
+  const [projectData, setProjectData] = useState(ProjectInfo);
+
+  useEffect(() => {
+    if (searchText === "") return;
+    setProjectData(() =>
+      ProjectInfo.filter((item) =>
+        item.name.toLowerCase().match(searchText.toLocaleLowerCase())
+      )
+    );
+  }, [searchText]);
+
+  function handleChange(e) {
+    e.preventDefault();
+    setSearchText(e.target.value);
+    if (!e.target.value.length > 0) {
+      setProjectData(ProjectInfo);
+    }
+  }
+
   return (
-    <div>
-      <h1>this is Projects page</h1>
-    </div>
+    <ProjectsStyles>
+      <div className="container">
+        <SectionTittle
+          heading="Projects"
+          subheading="some of my recent works"
+        />
+        <div className="projects__searchbar">
+          <form>
+            <input
+              type="text"
+              value={searchText}
+              onChange={handleChange}
+              placeholder="Project Name"
+            />
+            <MdSearch className="searchIcon" />
+          </form>
+        </div>
+        <div className="projects__allItems">
+          {projectData.map((item) => (
+            <ProjectItem
+              key={item.id}
+              title={item.name}
+              desc={item.desc}
+              img={item.img}
+            />
+          ))}
+        </div>
+      </div>
+    </ProjectsStyles>
   );
 }
