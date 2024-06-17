137f74fb4683edfdc35d7f717501c54349f9db8e
0xllordi <75077473+Lundrim23@users.noreply.github.com>
2024-06-17 23:13:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,9 +15,6 @@ export default function App() {
         <NavMenu />
         <ScrollToTop></ScrollToTop>
         <Routes>
-          
-        </Routes>
-        <Routes>
           <Route path="/Projects" element={<Projects />} />
         </Routes>
         <Routes>
