b7fd96066bf737a199893dc684411a2a184a438c
0xllordi <75077473+Lundrim23@users.noreply.github.com>
2024-06-17 23:13:24 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
-import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
 import TestimonialSection from "../components/TestimonialSection";
 import ContactBanner from "../components/ContactBanner";
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <div>
       <HeroSection />
-      <AboutSection />
       <ServicesSection />
       <ProjectSection />
       <TestimonialSection />
